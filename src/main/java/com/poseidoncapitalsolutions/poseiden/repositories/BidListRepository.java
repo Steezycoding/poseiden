453d94207be580262edea2240a2c25c2986948f1
Michael "Steezy" A. <38781991+Steezycoding@users.noreply.github.com>
2025-01-22 17:24:29 +0100

    :tada: Initialize project from Poseidon-skeleton

---
+++ src/main/java/com/poseidoncapitalsolutions/poseiden/repositories/BidListRepository.java
@@ -0,0 +1,9 @@
+package com.poseidoncapitalsolutions.poseiden.repositories;
+
+import com.nnk.springboot.domain.BidList;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+public interface BidListRepository extends JpaRepository<BidList, Integer> {
+
+}
---
+++ src/main/java/com/poseidoncapitalsolutions/poseiden/repositories/TradeRepository.java
@@ -0,0 +1,8 @@
+package com.poseidoncapitalsolutions.poseiden.repositories;
+
+import com.nnk.springboot.domain.Trade;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+public interface TradeRepository extends JpaRepository<Trade, Integer> {
+}

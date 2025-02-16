80ac665c77b7be36c691bcb38dd1a99c693923cf
Michael "Steezy" A. <38781991+Steezycoding@users.noreply.github.com>
2025-02-16 12:47:08 +0100

    :sparkles: Added CRUD create operation

--- src/test/java/com/poseidoncapitalsolutions/poseiden/repositories/BidTests.java
+++ src/test/java/com/poseidoncapitalsolutions/poseiden/repositories/BidTests.java
@@ -8,7 +8,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import java.util.List;
-import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -25,22 +24,11 @@ public class BidTests {
 
 		// Save
 		bid = bidListRepository.save(bid);
-		assertNotNull(bid.getBidListId());
+		assertNotNull(bid.getId());
 		assertEquals(bid.getBidQuantity(), 10d, 10d);
-
-		// Update
-		bid.setBidQuantity(20d);
-		bid = bidListRepository.save(bid);
-		assertEquals(bid.getBidQuantity(), 20d, 20d);
 
 		// Find
 		List<BidList> listResult = bidListRepository.findAll();
 		assertTrue(listResult.size() > 0);
-
-		// Delete
-		Integer id = bid.getBidListId();
-		bidListRepository.delete(bid);
-		Optional<BidList> bidList = bidListRepository.findById(id);
-		assertFalse(bidList.isPresent());
 	}
 }

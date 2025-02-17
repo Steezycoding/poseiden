9e784adf4d0747a17fc1b8694c0bcaf4e579b2cb
Michael "Steezy" A. <38781991+Steezycoding@users.noreply.github.com>
2025-02-17 07:19:35 +0100

    :sparkles: Added entity + CRUD read operation

--- src/main/java/com/poseidoncapitalsolutions/poseiden/controllers/CurveController.java
+++ src/main/java/com/poseidoncapitalsolutions/poseiden/controllers/CurveController.java
@@ -1,6 +1,10 @@
 package com.poseidoncapitalsolutions.poseiden.controllers;
 
 import com.poseidoncapitalsolutions.poseiden.domain.CurvePoint;
+import com.poseidoncapitalsolutions.poseiden.services.CurvePointService;
+import jakarta.validation.Valid;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -9,44 +13,54 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import jakarta.validation.Valid;
+import java.security.Principal;
 
 @Controller
+@RequestMapping("/curvePoint")
 public class CurveController {
-    // TODO: Inject Curve Point service
+    private final Logger logger = LoggerFactory.getLogger(CurveController.class);
 
-    @RequestMapping("/curvePoint/list")
-    public String home(Model model)
+    private final CurvePointService curvePointService;
+
+	public CurveController(CurvePointService curvePointService) {
+		this.curvePointService = curvePointService;
+	}
+
+	@RequestMapping("/list")
+    public String home(Model model, Principal principal)
     {
-        // TODO: find all Curve Point, add to model
+        if (principal != null) {
+            model.addAttribute("user", principal.getName());
+        }
+        model.addAttribute("curvePoints", curvePointService.getAll());
         return "curvePoint/list";
     }
 
-    @GetMapping("/curvePoint/add")
+    @GetMapping("/add")
     public String addBidForm(CurvePoint bid) {
         return "curvePoint/add";
     }
 
-    @PostMapping("/curvePoint/validate")
+    @PostMapping("/validate")
     public String validate(@Valid CurvePoint curvePoint, BindingResult result, Model model) {
         // TODO: check data valid and save to db, after saving return Curve list
         return "curvePoint/add";
     }
 
-    @GetMapping("/curvePoint/update/{id}")
+    @GetMapping("/update/{id}")
     public String showUpdateForm(@PathVariable("id") Integer id, Model model) {
         // TODO: get CurvePoint by Id and to model then show to the form
         return "curvePoint/update";
     }
 
-    @PostMapping("/curvePoint/update/{id}")
+    @PostMapping("/update/{id}")
     public String updateBid(@PathVariable("id") Integer id, @Valid CurvePoint curvePoint,
                              BindingResult result, Model model) {
         // TODO: check required fields, if valid call service to update Curve and return Curve list
         return "redirect:/curvePoint/list";
     }
 
-    @GetMapping("/curvePoint/delete/{id}")
+    @GetMapping("/delete/{id}")
     public String deleteBid(@PathVariable("id") Integer id, Model model) {
         // TODO: Find Curve by Id and delete the Curve, return to Curve list
         return "redirect:/curvePoint/list";
--- src/main/java/com/poseidoncapitalsolutions/poseiden/domain/CurvePoint.java
+++ src/main/java/com/poseidoncapitalsolutions/poseiden/domain/CurvePoint.java
@@ -1,17 +1,31 @@
 package com.poseidoncapitalsolutions.poseiden.domain;
 
-import org.hibernate.validator.constraints.Length;
-
 import jakarta.persistence.*;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import java.sql.Timestamp;
 
-
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 @Entity
 @Table(name = "curvepoint")
 public class CurvePoint {
 	@Id
-	@GeneratedValue(strategy= GenerationType.AUTO)
+	@GeneratedValue(strategy= GenerationType.IDENTITY)
 	private Integer id;
+
+	@Column(name="curve_id") private Integer curveId;
+	@Column(name="as_of_date") private Timestamp asOfDate;
+	private Double term;
+	private Double value;
+	@Column(name="creation_date") private Timestamp creationDate;
+
+	public CurvePoint(Integer curveId, Double term, Double value) {
+		this.curveId = curveId;
+		this.term = term;
+		this.value = value;
+	}
 }

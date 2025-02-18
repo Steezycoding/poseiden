f1d24f361ee9a5a0265215e9b0080be9c24735c6
Michael "Steezy" A. <38781991+Steezycoding@users.noreply.github.com>
2025-02-18 22:05:57 +0100

    Handle User CRUD operations
    * :recycle: Used Lombok on entity
    
    * :recycle: Cleaned controller requests mapping
    
    * :wrench: Fixed Lombok compilation issue + added devtools for hot swapping
    
    * :sparkles: Updated User find + create operations
    
    * :sparkles: Updated CRUD update operations
    
    * :sparkles: Updated CRUD delete operations
    
    * :bug: Fixed id value on update.html + findByUsername handling
    
    * :bug: Fixed password encryption on update operation

--- src/main/java/com/poseidoncapitalsolutions/poseiden/controllers/UserController.java
+++ src/main/java/com/poseidoncapitalsolutions/poseiden/controllers/UserController.java
@@ -1,76 +1,92 @@
 package com.poseidoncapitalsolutions.poseiden.controllers;
 
+import com.poseidoncapitalsolutions.poseiden.controllers.dto.UserDTO;
 import com.poseidoncapitalsolutions.poseiden.domain.User;
-import com.poseidoncapitalsolutions.poseiden.repositories.UserRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import com.poseidoncapitalsolutions.poseiden.exceptions.UserAlreadyExistsException;
+import com.poseidoncapitalsolutions.poseiden.exceptions.UserIdNotFoundException;
+import com.poseidoncapitalsolutions.poseiden.services.UserService;
+import jakarta.validation.Valid;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-
-import jakarta.validation.Valid;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
+@RequestMapping("/user")
 public class UserController {
-    @Autowired
-    private UserRepository userRepository;
+    private final Logger logger = LoggerFactory.getLogger(UserController.class);
 
-    @RequestMapping("/user/list")
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+
+    @GetMapping("/list")
     public String home(Model model)
     {
-        model.addAttribute("users", userRepository.findAll());
+        model.addAttribute("users", userService.getAll());
         return "user/list";
     }
 
-    @GetMapping("/user/add")
-    public String addUser(User bid) {
+    @GetMapping("/add")
+    public String addUser(Model model) {
+        model.addAttribute("user", new UserDTO());
         return "user/add";
     }
 
-    @PostMapping("/user/validate")
-    public String validate(@Valid User user, BindingResult result, Model model) {
-        if (!result.hasErrors()) {
-            BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
-            user.setPassword(encoder.encode(user.getPassword()));
-            userRepository.save(user);
-            model.addAttribute("users", userRepository.findAll());
-            return "redirect:/user/list";
-        }
-        return "user/add";
-    }
-
-    @GetMapping("/user/update/{id}")
-    public String showUpdateForm(@PathVariable("id") Integer id, Model model) {
-        User user = userRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("Invalid user Id:" + id));
-        user.setPassword("");
-        model.addAttribute("user", user);
-        return "user/update";
-    }
-
-    @PostMapping("/user/update/{id}")
-    public String updateUser(@PathVariable("id") Integer id, @Valid User user,
-                             BindingResult result, Model model) {
+    @PostMapping("/validate")
+    public String validate(@Valid @ModelAttribute("user") UserDTO userDTO, BindingResult result) {
         if (result.hasErrors()) {
-            return "user/update";
+            logger.error("User Registration form has errors {}", result.getAllErrors());
+            return "user/add";
+        } else {
+            userService.save(userDTO);
         }
-
-        BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
-        user.setPassword(encoder.encode(user.getPassword()));
-        user.setId(id);
-        userRepository.save(user);
-        model.addAttribute("users", userRepository.findAll());
         return "redirect:/user/list";
     }
 
-    @GetMapping("/user/delete/{id}")
+    @GetMapping("/update/{id}")
+    public String showUpdateForm(@PathVariable("id") Integer id, Model model) {
+        User user = userService.getById(id);
+        UserDTO userDTO = new UserDTO().fromEntity(user);
+
+        model.addAttribute("user", userDTO);
+        return "user/update";
+    }
+
+    @PostMapping("/update/{id}")
+    public String updateUser(@PathVariable("id") Integer id, @ModelAttribute("user") UserDTO userDTO, BindingResult result, Model model) {
+        if (result.hasErrors()) {
+            return "user/update";
+        }
+        userService.update(id, userDTO);
+
+        return "redirect:/user/list";
+    }
+
+    @DeleteMapping("/delete/{id}")
     public String deleteUser(@PathVariable("id") Integer id, Model model) {
-        User user = userRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("Invalid user Id:" + id));
-        userRepository.delete(user);
-        model.addAttribute("users", userRepository.findAll());
+        userService.delete(id);
+        model.addAttribute("users", userService.getAll());
+        return "redirect:/user/list";
+    }
+
+    // Handle exceptions
+    @ExceptionHandler(UserAlreadyExistsException.class)
+    public String handleInvalidUserFormException(UserAlreadyExistsException e, Model model) {
+        logger.error("Failed operation on user: {}", e.getMessage());
+        model.addAttribute("error", e.getMessage());
+        return "user/add";
+    }
+
+    @ExceptionHandler(UserIdNotFoundException.class)
+    public String handleUserNotFoundException(UserIdNotFoundException e, Model model) {
+        logger.error("Failed operation on user: {}", e.getMessage());
+        model.addAttribute("error", e.getMessage());
         return "redirect:/user/list";
     }
 }

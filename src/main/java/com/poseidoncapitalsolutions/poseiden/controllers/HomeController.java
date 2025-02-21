6fc9a73d7cc3e9d142afaab0709f178fd46f8a07
Michael "Steezy" A. <38781991+Steezycoding@users.noreply.github.com>
2025-02-21 18:16:09 +0100

    :recycle: Refactored home endpoint security

--- src/main/java/com/poseidoncapitalsolutions/poseiden/controllers/HomeController.java
+++ src/main/java/com/poseidoncapitalsolutions/poseiden/controllers/HomeController.java
@@ -1,5 +1,8 @@
 package com.poseidoncapitalsolutions.poseiden.controllers;
 
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,14 +13,19 @@ public class HomeController
 	@RequestMapping("/")
 	public String home(Model model)
 	{
-		return "home";
+		Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+		if (authentication.getAuthorities().contains(new SimpleGrantedAuthority("ROLE_ADMIN"))) {
+			return "redirect:/admin/home";
+		} else if (authentication.getAuthorities().contains(new SimpleGrantedAuthority("ROLE_USER"))) {
+			return "redirect:/bidList/list";
+		} else {
+			return "redirect:/app/login";
+		}
 	}
 
 	@RequestMapping("/admin/home")
 	public String adminHome(Model model)
 	{
-		return "redirect:/bidList/list";
+		return "home";
 	}
-
-
 }

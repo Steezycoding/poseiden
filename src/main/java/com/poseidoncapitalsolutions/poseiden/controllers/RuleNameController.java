1bd86467d2a27a145176702d0798e3accf2f6569
Michael "Steezy" A. <38781991+Steezycoding@users.noreply.github.com>
2025-02-24 22:34:47 +0100

    Handle Rule CRUD operations
    * :sparkles: Added entity + CRUD read operation
    
    * :sparkles: Added CRUD create operation
    
    * :sparkles: Added CRUD update operation
    
    * :sparkles: Added CRUD delete operation
    
    * :lipstick: Updated 'Rules' link title

--- src/main/java/com/poseidoncapitalsolutions/poseiden/controllers/RuleNameController.java
+++ src/main/java/com/poseidoncapitalsolutions/poseiden/controllers/RuleNameController.java
@@ -1,54 +1,84 @@
 package com.poseidoncapitalsolutions.poseiden.controllers;
 
+import com.poseidoncapitalsolutions.poseiden.controllers.dto.RuleNameDTO;
 import com.poseidoncapitalsolutions.poseiden.domain.RuleName;
+import com.poseidoncapitalsolutions.poseiden.services.RuleNameService;
+import jakarta.persistence.EntityNotFoundException;
+import jakarta.validation.Valid;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
-import jakarta.validation.Valid;
+import java.security.Principal;
 
 @Controller
 public class RuleNameController {
-    // TODO: Inject RuleName service
+    private final Logger logger = LoggerFactory.getLogger(RuleNameController.class);
+
+    private final RuleNameService ruleNameService;
+
+    public RuleNameController(RuleNameService ruleNameService) {
+        this.ruleNameService = ruleNameService;
+    }
 
     @RequestMapping("/ruleName/list")
-    public String home(Model model)
+    public String home(Model model, Principal principal)
     {
-        // TODO: find all RuleName, add to model
+        if (principal != null) {
+            model.addAttribute("user", principal.getName());
+        }
+        model.addAttribute("ruleNames", ruleNameService.getAll());
         return "ruleName/list";
     }
 
     @GetMapping("/ruleName/add")
-    public String addRuleForm(RuleName bid) {
+    public String addRuleForm(Model model) {
+        model.addAttribute("ruleName", new RuleNameDTO());
         return "ruleName/add";
     }
 
     @PostMapping("/ruleName/validate")
-    public String validate(@Valid RuleName ruleName, BindingResult result, Model model) {
-        // TODO: check data valid and save to db, after saving return RuleName list
-        return "ruleName/add";
+    public String validate(@Valid @ModelAttribute("ruleName") RuleNameDTO ruleName, BindingResult result) {
+        if (result.hasErrors()) {
+            logger.error("RuleName Add form has errors {}", result.getAllErrors());
+            return "ruleName/add";
+        }
+        ruleNameService.save(ruleName);
+        return "redirect:/ruleName/list";
     }
 
     @GetMapping("/ruleName/update/{id}")
     public String showUpdateForm(@PathVariable("id") Integer id, Model model) {
-        // TODO: get RuleName by Id and to model then show to the form
+        RuleName ruleName = ruleNameService.getById(id);
+        RuleNameDTO ruleNameDTO = new RuleNameDTO().fromEntity(ruleName);
+
+        model.addAttribute("ruleName", ruleNameDTO);
         return "ruleName/update";
     }
 
     @PostMapping("/ruleName/update/{id}")
-    public String updateRuleName(@PathVariable("id") Integer id, @Valid RuleName ruleName,
-                             BindingResult result, Model model) {
-        // TODO: check required fields, if valid call service to update RuleName and return RuleName list
+    public String updateRuleName(@PathVariable("id") Integer id, @Valid @ModelAttribute("ruleName") RuleNameDTO ruleNameDTO, BindingResult result) {
+        if (result.hasErrors()) {
+            logger.error("RuleName Update form has errors {}", result.getAllErrors());
+            return "ruleName/update";
+        }
+        ruleNameService.update(ruleNameDTO);
         return "redirect:/ruleName/list";
     }
 
     @GetMapping("/ruleName/delete/{id}")
     public String deleteRuleName(@PathVariable("id") Integer id, Model model) {
-        // TODO: Find RuleName by Id and delete the RuleName, return to Rule list
+        ruleNameService.delete(id);
+        return "redirect:/ruleName/list";
+    }
+
+    @ExceptionHandler(EntityNotFoundException.class)
+    public String handleEntityNotFoundException(EntityNotFoundException e, Model model) {
+        logger.error("Failed operation on trade: {}", e.getMessage());
+        model.addAttribute("error", e.getMessage());
         return "redirect:/ruleName/list";
     }
 }
